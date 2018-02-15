4cdcc0dd862ada0ce75ecd60d9e8fa577aaeb588
bibu <bibu01@yandex.ru>
2018-02-15 15:10:28 +0300

    теперь рабочий, но делает не то что надо

--- images/images.py
+++ images/images.py
@@ -1,9 +1,9 @@
 import requests
 
 
-url - 'https://dota2.ru/img/heroes/abaddon/m_icon.jpg'
+url = 'https://dota2.ru/img/heroes/abaddon/icon.jpg'
 
-r - requests.get(url, stream=True)
+r = requests.get(url, stream=True)
 
 with open('1.jpg', 'bw') as f:
     for chunk in r.iter_content(8192):
