23d5ac446106e996e7c0c6ef1d811b6ddd43bccb
bibu <bibu01@yandex.ru>
2018-02-16 02:40:15 +0300

    теперь работает правильно, но не полностью

--- images/images.py
+++ images/images.py
@@ -1,10 +1,58 @@
 import requests
 
 
-url = 'https://dota2.ru/img/heroes/abaddon/icon.jpg'
+urlt = 'https://dota2.ru/img/heroes/abaddon/m_icon.jpg'
 
-r = requests.get(url, stream=True)
+urlt2='axe'
 
-with open('1.jpg', 'bw') as f:
-    for chunk in r.iter_content(8192):
-        f.write(chunk)
+urls = {
+    'abaddon',
+    'alchemist',
+    'axe',
+    'beastmaster',
+    'brewmaster',
+    'bristleback',
+    'centaur_warruner',
+    'chaos_knight',
+    'clockwerk',
+    'doom',
+    'dragon_knight',
+    'earth_spirit',
+    'earthshaker',
+    'elder_titan',
+    'huskar',
+    'io',
+    'kunkka',
+    'legion_commander',
+    'lifestealer',
+    'lykan',
+    'magnus',
+    'night_stalker',
+    'omniknight',
+    'phoenix',
+    'pudge',
+    'sand_king',
+    'slardar',
+    'spirit_breaker',
+    'sven',
+    'tidehunter',
+    'timbersaw',
+    'tiny',
+    'treant_protektor',
+    'tusk',
+    'underlord',
+    'undying',
+    'wraithking',
+    }
+
+
+for url in urls:
+    s = f'https://dota2.ru/img/heroes/{url}/m_icon.jpg'
+    r = requests.get(s, stream=True)
+    a = 1
+    with open(url+'.jpg', 'bw') as f:
+        for chunk in r.iter_content(8192):
+            f.write(chunk);
+    a= a+1        
+
+    
